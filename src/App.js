85095d7f1c02f70b345e8c7572bb9a9519b2052d
thesmitpatel <61879629+thesmitpatel@users.noreply.github.com>
2023-11-01 21:50:07 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import "./App.css";
+import { ThemeProvider } from "styled-components";
+import styled from "styled-components";
+import { darkTheme } from "./utils/themes";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Herosection";
+import Education from "./components/Education";
+import skills from "./components/Skills";
+import { BrowserRouter as Router } from "react-router-dom";
+import Skills from "./components/Skills";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1>Your App</h1>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+        <Navbar />
+        <Body>
+          <Hero />
+          <Wrapper>
+            <Skills />
+            <Education />
+          </Wrapper>
+        </Body>
+      </Router>
+    </ThemeProvider>
   );
 }
 
